a2843bbda9902d85e68fbca85404dad78b6350e5
nasser25 <nasseremil25@gmail.com>
2024-06-19 14:21:53 -0400

    search schedule repository

--- Movilissa-api/Data/Repositories/SearchRepository.cs
+++ Movilissa-api/Data/Repositories/SearchRepository.cs
@@ -11,10 +11,16 @@ public class SearchRepository
     {
         _context = context;
     }
-    public async Task<List<Schedule>> SearchSchedulesAsync(int origin, int destination, DateTime? date, int company)
-    {
-        return await _context.Schedules.ToListAsync();
 
-    }
-
+    public async Task<List<Schedule>> SearchSchedulesAsync(int origin, int destination, DateTime? date, int company) =>
+       await _context.Schedules
+            .Include(s => s.Route)
+            .ThenInclude(r => r.Origin)
+            .Include(s => s.Route)
+            .ThenInclude(r => r.Destination)
+            .Where(s => s.Route.OriginId == origin)
+            .Where(s => s.Route.DestinationId == destination)
+            .Where(s => company == null || s.CompanyId == company)
+            // .Where(s => s.date == date)
+            .ToListAsync();
 }
