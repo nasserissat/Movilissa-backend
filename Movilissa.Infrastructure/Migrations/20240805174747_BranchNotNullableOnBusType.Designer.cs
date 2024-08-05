880c0c8407693608a0dbe42ff7cf3706d0059721
nasser25 <nasseremil25@gmail.com>
2024-08-05 13:50:36 -0400

    BrandNotNullableOnBusType migration

--- Movilissa.Infrastructure/Migrations/20240805174747_BranchNotNullableOnBusType.Designer.cs
+++ Movilissa.Infrastructure/Migrations/20240805174747_BranchNotNullableOnBusType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movilissa_api.Data.Context;
 
@@ -11,9 +12,11 @@ using Movilissa_api.Data.Context;
 namespace Movilissa.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240805174747_BranchNotNullableOnBusType")]
+    partial class BranchNotNullableOnBusType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Movilissa.Infrastructure/Migrations/20240805174747_BranchNotNullableOnBusType.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movilissa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class BranchNotNullableOnBusType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BrandId",
+                table: "BusTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BusTypes_Brand_BrandId",
+                table: "BusTypes",
+                column: "BrandId",
+                principalTable: "Brand",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BrandId",
+                table: "BusTypes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BusTypes_Brand_BrandId",
+                table: "BusTypes",
+                column: "BrandId",
+                principalTable: "Brand",
+                principalColumn: "Id");
+        }
+    }
+}
